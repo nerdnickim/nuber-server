8f4c4f518863646172bc209ccc4cd2381207b54c
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-07-17 22:10:53 +0900

    ride entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -68,6 +68,7 @@ class User extends BaseEntity {
 	}
 
 	@CreateDateColumn() createdAt: string;
+
 	@UpdateDateColumn() updatedAt: string;
 
 	public comparePassword(password: string): Promise<boolean> {

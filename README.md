3c08ff4ee610bfd7ee07b44c9c44352e253813a5
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-07-19 16:16:56 +0900

    deleteplace

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Clone (N)uber-server Graphql, NodeJs, TypeScript
 - [x] Toggle Driving Mode
 - [x] Report Location / Orientation
 - [x] Add Place
-- [ ] Edit Place
+- [x] Edit Place
 - [ ] Delete Place
 - [ ] Get My Places
 - [ ] See Nearby Drivers
---
+++ src/api/Place/DeletePlace/DeletePlace.resolvers.ts
@@ -0,0 +1,44 @@
+import { Resolvers } from "src/types/resolvers";
+import privateResolver from "src/utils/privateResolver";
+import User from "src/entities/User";
+import { DeletePlaceMutationArgs, DeletePlaceResponse } from "../../../types/graph";
+import Place from "src/entities/Place";
+
+const resolvers: Resolvers = {
+	Mutation: {
+		DeletePlace: privateResolver(
+			async (_, args: DeletePlaceMutationArgs, { req }): Promise<DeletePlaceResponse> => {
+				const user: User = req.user;
+				try {
+					const place = await Place.findOne({ id: args.placeId });
+					if (place) {
+						if (place.userId === user.id) {
+							place.remove();
+							return {
+								ok: true,
+								error: null,
+							};
+						} else {
+							return {
+								ok: false,
+								error: "Not Authorized",
+							};
+						}
+					} else {
+						return {
+							ok: false,
+							error: "Place not found",
+						};
+					}
+				} catch (error) {
+					return {
+						ok: false,
+						error: error.message,
+					};
+				}
+			}
+		),
+	},
+};
+
+export default resolvers;
